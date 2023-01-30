85f88a0afb4ecf0877aeb7dc4a6be8ba9af82cd3
Awele Lan <awelelan@Aweles-MacBook-Air.local>
2023-01-30 09:41:07 -0600

    this

--- script.js
+++ script.js
@@ -1,8 +1,60 @@
 //user clicks a button
   //event listener, calling a function to write password
+
+
   //inside our function we prompt for password length
-    //var passwordLength = prompt("How many characters would you like your password to contain?"); //what does prompt return??
-      //we need to validate the prompt so that it meets criteria, must be a number, gt 8 lt 129
+function generatePassword() {
+  var passwordLength = prompt("How many characters would you like your password to contain?"); //what does prompt return??
+
+  if (passwordLength < 8) {
+    alert('Password length must be at least 8 characters');
+  } else if (passwordLength > 128) {
+    alert('Password length must be no more than 128 characters');
+  } else {
+    var hasNumeric = confirm("Click OK to confirm including numeric characters.");
+    var hasLower = confirm("Click OK to confirm including Lower characters.");
+    var hasUpper = confirm("Click OK to confirm including Upper characters.");
+    var hasSpecial = confirm("Click OK to confirm including Special characters.");
+
+    if(hasNumeric === false && hasLower === false && hasUpper === false && hasSpecial === false){
+      alert('Must select at least one character type');
+    } else {var userPassword = {
+          length: passwordLength,
+          hasNumeric: hasNumeric,
+          hasLower: hasLower,
+          hasUpper: hasUpper,
+          hasSpecial: hasSpecial,
+      }
+       return userPassword;
+}
+  }
+}
+function generateuserpassword (userOptions){
+  var possibleChars = [];
+  var result = [];
+  var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+  var LowerCharacters = ['a', 'b', 'c', ];
+  var upperCharacters = [];
+  var specialCharacters = [];
+
+  if(userOptions.hasNumeric === true){
+  possibleChars = possibleChars.concat(numericCharacters);
+  }
+
+if(userOptions.hasLower === true){
+  possibleChars = possibleChars.concat(LowerCharacters);
+}
+  if(userOptions.hasUpper === true){
+    possibleChars = possibleChars.concat(upperCharacters);
+    }
+    if(userOptions.hasSpecial === true){
+      possibleChars = possibleChars.concat(specialCharacters);
+      }
+
+      // fill the results array with 
+  } 
+  //var passwordLength = prompt("How many characters would you like your password to contain?"); //what does prompt return??
+      //we need to validate the prompt so that it meets criteria, must be a number, greater than 8 less than 129
     //then user is prompted password length 
 
     //if(passwordLength < 8){
@@ -44,16 +96,16 @@
 
 
 // // Assignment Code
-// var generateBtn = document.querySelector("#generate");
+var generateBtn = document.querySelector("#generate");
 
-// // Write password to the #password input
-// function writePassword() {
-//   var password = generatePassword();
-//   var passwordText = document.querySelector("#password");
 
-//   passwordText.value = password;
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
 
-// }
+  passwordText.value = password;
 
-// // Add event listener to generate button
-// generateBtn.addEventListener("click", writePassword);
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
